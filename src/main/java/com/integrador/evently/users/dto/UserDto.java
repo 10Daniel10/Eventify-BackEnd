a6eb867dd97c84d2cdfb52bdd8a93b610b6e6d4b
salvadiaz <salvador.diaz@bluealba.com>
2023-11-29 12:07:22 -0300

    update endpoints

--- src/main/java/com/integrador/evently/users/dto/UserDto.java
+++ src/main/java/com/integrador/evently/users/dto/UserDto.java
@@ -5,7 +5,6 @@ import lombok.Data;
 @Data
 public class UserDto {
     private Long id;
-    private String username;
     private String email;
     private String firstname;
     private String lastname;
