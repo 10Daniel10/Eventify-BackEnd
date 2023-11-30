ab2aed372964c186a6b885b21b963a18aef5491b
federico bustamante <fede.dhouse@gmail.com>
2023-11-30 20:33:17 -0300

    added @CrossOrigin

--- src/main/java/com/integrador/evently/pdfReport/controller/PdfController.java
+++ src/main/java/com/integrador/evently/pdfReport/controller/PdfController.java
@@ -4,6 +4,7 @@ import com.integrador.evently.pdfReport.service.PdfService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.io.ByteArrayOutputStream;
 
 @RestController
 @RequestMapping("/report")
+@CrossOrigin
 public class PdfController {
     private final PdfService pdfService;
 
